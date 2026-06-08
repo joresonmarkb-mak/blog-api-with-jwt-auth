57ec0c5f57bb519ba86813ee3caf023956092667
John Doe <mac.jar16@gmail.com>
2026-06-09 07:43:11 +0800

    fix index

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
-import dotenv from "dotenv";
-import { fileURLToPath } from 'url';
-import { dirname, join} from 'path'
-import connectDB from "./config/database.js";
-import app from "./app.js"
+import dotenv from 'dotenv';
+import connectDB from './config/database.js';
+import app from './app.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname (__filename);
+dotenv.config(); // works both locally and on Render
 
-
-
-dotenv.config({
-    path: join(__dirname, '../../.env')
-});
-
-const startServer = async() => {
-    try {
-        await connectDB();
-
-        app.on("error", (error)=>{
-            console.log("error", error);
-            throw error;
-        }); 
-        app.listen(process.env.PORT || 8000, ()=>{
-            console.log(`Server is running on port: ${process.env.PORT || 8000} `);
-
-        })
-    } catch (error) {
-        console.log("MongoDB connection failed", error);
-    }
-}
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server running on port ${process.env.PORT || 4000}`);
+    });
+  } catch (error) {
+    console.log('MongoDB connection failed', error);
+  }
+};
 
 startServer();
